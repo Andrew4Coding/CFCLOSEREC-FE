f68a9d3f9ff674a84832ebc7a337e532e7234729
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-01-25 16:03:46 +0800

    error resolved

--- src/app/account/page.tsx
+++
@@ -1,6 +0,0 @@
-import { AccountModule } from "@/components/modules/AccountModules";
-import { NextPage } from "next";
-
-export const AccountPage: NextPage = () => <AccountModule />
-
-export default AccountPage;
